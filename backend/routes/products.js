a558c590be65cbcc7fc8bc42cf3f4ada9f27fede
Arun kashyap <arunkashyap230@gmail.com>
2025-07-29 13:37:13 +0530

    routes

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -3,7 +3,13 @@ const express = require("express");
 const router = express.Router();
 const { addProduct } = require("../controllers/productController");
 const upload = require("../middleware/cloudinaryUpload");
+const Product = require("../models/Product");
 
 router.post("/upload", upload.single("image"), addProduct);
+
+router.get("/", async (req, res) => {
+  const products = await Product.find();
+  res.json({ products });
+});
 
 module.exports = router;
